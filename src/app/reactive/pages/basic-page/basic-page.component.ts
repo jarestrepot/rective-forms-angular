93ade6f9b82abbf8756c54b9dddb5187af54bf64
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-01-15 22:11:46 +0100

    First form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators , FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  // public myForm: FormGroup = new FormGroup({
+  //   nama: new FormControl('', [], []),
+  //   price: new FormControl(0, [], []),
+  //   inStorage: new FormControl(0, [], [])
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: [ '', [ Validators.required , Validators.minLength(3) ], [] ],
+    price: [ 0, [ Validators.required , Validators.min(0) ], [] ],
+    inStorage: [0, [ Validators.required, Validators.min(0) ], [] ],
+  })
+
+  // Form Builder
+  constructor( private fb: FormBuilder ){}
+
+  onSave():void{
+    if( this.myForm.invalid ) return;
+    console.log(this.myForm.value);
+  }
 }
