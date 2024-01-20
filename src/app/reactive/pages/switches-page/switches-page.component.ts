21f9ce9f9502c72ed02e2d0d49622700056a362a
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-01-20 16:37:21 +0100

    Checkbox, radius form

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Person {
+  gender: string,
+  wantNotifications: boolean
+}
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termAndConditions: [ true, Validators.requiredTrue ],
+  });
+
+  @ViewChild('invalidMessage')
+  public messageError!: ElementRef<HTMLSpanElement>;
+  public mockPerson:Person = {
+    gender: 'F',
+    wantNotifications: false
+  }
+
+  constructor( private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset( this.mockPerson );
+  }
+
+  onSave(){
+    if( this.myForm.valid ){
+      const { termAndConditions, ...newPerson } = this.myForm.value;
+      this.mockPerson = newPerson;
+      this.myForm.reset( this.mockPerson );
+      return ;
+    }
+    this.myForm.markAllAsTouched();
+  }
+
+  isValidRadius( fieldRadius: string ):boolean{
+    return Boolean( this.myForm.controls[fieldRadius].errors );
+  }
+
+  isValidField( field:string ):boolean {
+    return Boolean( this.myForm.touched && this.myForm.controls[ field ].errors );
+  }
 }
